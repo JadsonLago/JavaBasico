b226e6356643d6cd1aedba72aca7b2e0d9ea221e
Jadson Lago <62965849+JadsonLago@users.noreply.github.com>
2023-03-01 12:45:28 +0000

    correcao_de_identacao

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-//import java.util.Locale;
 
 class Main {
   public static void main(String[] args) {
@@ -11,16 +10,16 @@ class Main {
     aliqInterno = 0.18;
     aliqInterestadual = 0.07;     
    
-
     System.out.println("Informe o valor do produto: ");
     valorProduto = sc.nextDouble();
     icmsNormal = valorProduto * aliqInterno;
     totalNota = valorProduto + icmsNormal;
     icmsInterestadual = valorProduto * aliqInterestadual;
     icmsAntecipacao = icmsNormal - icmsInterestadual;
+    
     sc.close();
     
-    System.out.println("=======================================");
+    System.out.println("*=======================================*");
     System.out.println("                                       ");
     System.out.printf("Valor do produto:...........%.2f%n",valorProduto);
     System.out.printf("ICMS Normal:.................%.2f%n",icmsNormal);
